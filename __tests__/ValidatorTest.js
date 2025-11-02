6c180a211178af69aa26037451779f9858df7d70
BHyeonKim <rlaqhguse@gmail.com>
2025-11-03 04:05:03 +0900

    test(validator): Test validatePositiveNumber method

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -12,11 +12,31 @@ describe('Validator class test', () => {
       },
     );
 
-    it.each([[1][2][0][-1]])(
+    it.each([[1], [2], [0], [-1]])(
       'should pass test if arg is a number:(%s)',
       (arg) => {
         expect(() => Validator.validateNumber(arg)).not.toThrow(
           ERROR_MESSAGE.NOT_NUMBER,
+        );
+      },
+    );
+  });
+
+  describe('validatePositiveNumber method test', () => {
+    it.each([[0], [-1]])(
+      'should throw an error if arg is not a positive number:(%s)',
+      (arg) => {
+        expect(() => Validator.validatePositiveNumber(arg)).toThrow(
+          ERROR_MESSAGE.NOT_POSITIVE_NUMBER,
+        );
+      },
+    );
+
+    it.each([[1], [2]])(
+      'should pass test if arg is a positive number:(%s)',
+      (arg) => {
+        expect(() => Validator.validatePositiveNumber(arg)).not.toThrow(
+          ERROR_MESSAGE.NOT_POSITIVE_NUMBER,
         );
       },
     );
