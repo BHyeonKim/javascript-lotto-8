f195f021dda04663a2d445034e4bb2d465490557
BHyeonKim <rlaqhguse@gmail.com>
2025-11-03 04:01:35 +0900

    test(validator): Test validateNumber method

---
+++ __tests__/ValidatorTest.js
@@ -0,0 +1,24 @@
+import ERROR_MESSAGE from '../src/constants/error.js';
+import Validator from '../src/utils/Validator.js';
+
+describe('Validator class test', () => {
+  describe('validateNumber method test', () => {
+    it.each([[' '], [true], [{}], [[]], [Symbol], [null], [undefined]])(
+      'should throw an error if arg is not a number:(%s)',
+      (arg) => {
+        expect(() => Validator.validateNumber(arg)).toThrow(
+          ERROR_MESSAGE.NOT_NUMBER,
+        );
+      },
+    );
+
+    it.each([[1][2][0][-1]])(
+      'should pass test if arg is a number:(%s)',
+      (arg) => {
+        expect(() => Validator.validateNumber(arg)).not.toThrow(
+          ERROR_MESSAGE.NOT_NUMBER,
+        );
+      },
+    );
+  });
+});
