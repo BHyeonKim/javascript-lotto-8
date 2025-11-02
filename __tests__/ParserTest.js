0cbc6003e0cfdea2cb1176fcf63a2b9b6f980be5
BHyeonKim <rlaqhguse@gmail.com>
2025-11-03 04:36:57 +0900

    test(parser): Test parseMoney method

--- __tests__/ParserTest.js
+++ __tests__/ParserTest.js
@@ -1,4 +1,5 @@
 import ERROR_MESSAGE from '../src/constants/error.js';
+import GAME_CONSTANT from '../src/constants/gameConstant.js';
 import Parser from '../src/utils/Parser.js';
 
 describe('Parser class test', () => {
@@ -17,6 +18,34 @@ describe('Parser class test', () => {
           ERROR_MESSAGE.NOT_NUMBER,
         );
         expect(typeof Parser.parseNumber(arg)).toBe('number');
+      },
+    );
+  });
+
+  describe('parseMoney method test', () => {
+    it.each([['0'], ['-1000'], ['-2000']])(
+      'should throw an error if arg is not a positive number:(%s)',
+      (arg) => {
+        expect(() => Parser.parseMoney(arg)).toThrow(
+          ERROR_MESSAGE.NOT_POSITIVE_NUMBER,
+        );
+      },
+    );
+
+    it.each([['500'], ['1500'], ['2100'], ['999']])(
+      `should throw an error if arg is not divisible by base unit (${GAME_CONSTANT.BASE_UNIT}):(%s)`,
+      (arg) => {
+        expect(() => Parser.parseMoney(arg)).toThrow(
+          ERROR_MESSAGE.NOT_DIVIDE_BY_BASE_UNIT,
+        );
+      },
+    );
+
+    it.each([['1000'], ['2000'], ['5000'], ['10000']])(
+      'should return a number if arg is valid money amount:(%s)',
+      (arg) => {
+        expect(() => Parser.parseMoney(arg)).not.toThrow();
+        expect(typeof Parser.parseMoney(arg)).toBe('number');
       },
     );
   });
