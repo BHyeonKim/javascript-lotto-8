6e1ac619540a8d033d86048daf4c6016129b6305
BHyeonKim <rlaqhguse@gmail.com>
2025-11-03 03:05:11 +0900

    refactor(constant): Replace hardcoded lotto length

--- src/constants/error.js
+++ src/constants/error.js
@@ -1,5 +1,7 @@
+import GAME_CONSTANT from './gameConstant.js';
+
 const ERROR_MESSAGE = {
-  LOTTO_INVALID_LENGTH: '로또 번호는 6개여야 합니다',
+  LOTTO_INVALID_LENGTH: `로또 번호는 ${GAME_CONSTANT.LOTTO_LENGTH}개여야 합니다`,
   LOTTO_DUPLICATED_NUMBER: '로또 번호는 중복될 수 없습니다.',
 };
 
