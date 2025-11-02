32415665eae80565513d550b0ffacc5a453ef2f1
BHyeonKim <rlaqhguse@gmail.com>
2025-11-03 04:24:51 +0900

    test(validator): Test validateDivideByBaseUnit method

--- __tests__/ValidatorTest.js
+++ __tests__/ValidatorTest.js
@@ -1,4 +1,5 @@
 import ERROR_MESSAGE from '../src/constants/error.js';
+import GAME_CONSTANT from '../src/constants/gameConstant.js';
 import Validator from '../src/utils/Validator.js';
 
 describe('Validator class test', () => {
@@ -36,6 +37,26 @@ describe('Validator class test', () => {
       'should pass test if arg is a positive number:(%s)',
       (arg) => {
         expect(() => Validator.validatePositiveNumber(arg)).not.toThrow(
+          ERROR_MESSAGE.NOT_POSITIVE_NUMBER,
+        );
+      },
+    );
+  });
+
+  describe('validateDivideByBaseUnit method test', () => {
+    it.each([[900], [1500], [2100]])(
+      `should throw an error if arg is not divisible by ${GAME_CONSTANT.BASE_UNIT}`,
+      (arg) => {
+        expect(() => Validator.validateDivideByBaseUnit(arg)).toThrow(
+          ERROR_MESSAGE.NOT_DIVIDE_BY_BASE_UNIT,
+        );
+      },
+    );
+
+    it.each([[1000], [2000], [10000]])(
+      `should pass test if arg is not divisible by ${GAME_CONSTANT.BASE_UNIT}`,
+      (arg) => {
+        expect(() => Validator.validateDivideByBaseUnit(arg)).not.toThrow(
           ERROR_MESSAGE.NOT_POSITIVE_NUMBER,
         );
       },
