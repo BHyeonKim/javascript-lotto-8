0b131f7aa0177a6ae695a2a2da7b2c7e650f3fd5
BHyeonKim <rlaqhguse@gmail.com>
2025-11-03 14:09:52 +0900

    feat: Add constants

---
+++ src/constants/message.js
@@ -0,0 +1,7 @@
+const MESSAGE = {
+  ENTER_PURCHASE_AMOUNT: '구입금액을 입력해 주세요.\n',
+  ENTER_WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
+  ENTER_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
+};
+
+export default MESSAGE;
