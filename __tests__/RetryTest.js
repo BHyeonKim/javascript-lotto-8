af923329dfb7f4b0189ecdd4f0478a699881e409
BHyeonKim <rlaqhguse@gmail.com>
2025-11-03 17:56:20 +0900

    test(retry): Test retry logic

---
+++ __tests__/RetryTest.js
@@ -0,0 +1,45 @@
+import ERROR_MESSAGE from '../src/constants/error';
+import Retry from '../src/utils/retry';
+import View from '../src/View';
+
+describe('Retry class test', () => {
+  it('should retry if callback funcion rejected one time', async () => {
+    const callback = jest.fn().mockRejectedValueOnce().mockResolvedValueOnce();
+
+    await Retry.execute(callback);
+
+    expect(callback).toHaveBeenCalledTimes(2);
+  });
+
+  it('should retry if callback funcion rejected multiple times', async () => {
+    const callback = jest
+      .fn()
+      .mockRejectedValueOnce()
+      .mockRejectedValueOnce()
+      .mockRejectedValueOnce()
+      .mockResolvedValueOnce();
+
+    await Retry.execute(callback);
+
+    expect(callback).toHaveBeenCalledTimes(4);
+  });
+
+  it('should call callback function once if callback executed successfully', async () => {
+    const callback = jest.fn().mockResolvedValueOnce();
+
+    await Retry.execute(callback);
+
+    expect(callback).toHaveBeenCalledTimes(1);
+  });
+
+  it('should print error if execption occured', async () => {
+    const log = jest.spyOn(View, 'print');
+
+    const callback = jest.fn().mockRejectedValueOnce();
+
+    await Retry.execute(callback);
+
+    expect(log).toHaveBeenCalledWith(expect.stringContaining(ERROR_MESSAGE.PREFIX));
+    expect(callback).toHaveBeenCalledTimes(2)
+  });
+});
